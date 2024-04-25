31a038afd7d498fc710e4d2eaed52e30fccc2d7b
UTKARSH-PATIDAR <utkarsh.patidar@acquia.com>
2024-04-25 17:17:46 +0530

    Resturctured files

--- src/Components/form/Input.jsx
+++
@@ -1,9 +0,0 @@
-import {props} from "../utils";
-import inputBehaviors from "./inputBehaviors";
-function Input(properties) {
-
-const {attributes = {}, children = '', renderChildren = ''} = properties;
-   return <><input {...props(attributes)}  />{ renderChildren }</>
-}
-
-export default inputBehaviors(Input)
